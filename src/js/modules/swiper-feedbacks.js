81b04dd5ce37888cde3c8451ce07d8c51d7825ff
Prokuuudin <prokuuudin@gmail.com>
2025-03-15 13:42:05 +0200

    14

--- src/js/modules/swiper-feedbacks.js
+++ src/js/modules/swiper-feedbacks.js
@@ -4,7 +4,10 @@ import 'swiper/css/bundle';
 function getSwiperFeedback() {
     new Swiper (".swiper", {
       direction: 'horizontal',
+      slidesPerView: 1.3,
+      spaceBetween: 40,
       loop: true,
+      
       
       pagination: {
       el: '.swiper-pagination',
